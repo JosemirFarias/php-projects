8c274baae0d62df9a55b85d971ea19b8bc777899
JosemirFarias <josemirfbrito@gmail.com>
2025-07-25 18:27:01 -0300

    introdução à variáveis

--- inicio/index.php
+++ inicio/index.php
@@ -1,3 +1,14 @@
 <?php
-echo "Hello, World!";
-?>
+echo "Hello, World!<br>";
+echo "<br>";
+
+$nome = "Josemir";
+$idade = 36;
+$altura = 1.83;
+
+echo "Meu nome é $nome<br>";
+echo "<br>";
+echo "Tenho $idade de idade <br>";
+echo "<br>";
+echo "Tenho $altura de altura";
+?>  
